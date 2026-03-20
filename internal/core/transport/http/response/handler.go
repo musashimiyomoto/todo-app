63c4269941eb6b5d7068f6677cdea11eab01bab5
iosifkrokai <iosif.krokai@gmail.com>
2026-03-20 18:59:04 +0000

    feat: add validate request

--- internal/core/transport/http/response/handler.go
+++ internal/core/transport/http/response/handler.go
@@ -2,9 +2,11 @@ package core_http_response
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 
+	core_errors "github.com/musashimiyomoto/todo-app/internal/core/errors"
 	core_loger "github.com/musashimiyomoto/todo-app/internal/core/logger"
 	"go.uber.org/zap"
 )
@@ -18,11 +20,43 @@ func NewHTTPResponseHandler(log *core_loger.Logger, rw http.ResponseWriter) *HTT
 	return &HTTPResponseHandler{log: log, rw: rw}
 }
 
+func (h *HTTPResponseHandler) ErrorResponse(err error, msg string) {
+	var (
+		statusCode int
+		logFunc    func(string, ...zap.Field)
+	)
+
+	switch {
+	case errors.Is(err, core_errors.ErrInvalidArgument):
+		statusCode = http.StatusBadRequest
+		logFunc = h.log.Warn
+	case errors.Is(err, core_errors.ErrInvalidArgument):
+		statusCode = http.StatusNotFound
+		logFunc = h.log.Debug
+	case errors.Is(err, core_errors.ErrConflict):
+		statusCode = http.StatusConflict
+		logFunc = h.log.Warn
+	default:
+		statusCode = http.StatusInternalServerError
+		logFunc = h.log.Error
+	}
+
+	logFunc(msg, zap.Error(err))
+
+	h.errorResponse(statusCode, err, msg)
+}
+
 func (h *HTTPResponseHandler) PanicResponse(p any, msg string) {
 	statusCode := http.StatusInternalServerError
+
 	err := fmt.Errorf("Unexpected panic: %v", p)
 
 	h.log.Error(msg, zap.Error(err))
+
+	h.errorResponse(statusCode, err, msg)
+}
+
+func (h *HTTPResponseHandler) errorResponse(statusCode int, err error, msg string) {
 	h.rw.WriteHeader(statusCode)
 
 	response := map[string]string{
--- internal/features/users/transport/http/create_user.go
+++ internal/features/users/transport/http/create_user.go
@@ -1,16 +1,16 @@
 package users_transport_http
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
 
 	core_loger "github.com/musashimiyomoto/todo-app/internal/core/logger"
+	core_http_request "github.com/musashimiyomoto/todo-app/internal/core/transport/http/request"
+	core_http_response "github.com/musashimiyomoto/todo-app/internal/core/transport/http/response"
 )
 
 type CreateUserRequest struct {
-	FullName    string  `json:"full_name"`
-	PhoneNumber *string `json:"phone_number"`
+	FullName    string  `json:"full_name" validate:"required,min=3,max=100"`
+	PhoneNumber *string `json:"phone_number" validate:"omitempty,min=10,max=15,startsWith=+"`
 }
 
 type CreateUserResponse struct {
@@ -23,12 +23,13 @@ type CreateUserResponse struct {
 func (h *UsersHTTPHandler) CreateUser(rw http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 	log := core_loger.FromContext(ctx)
+	responseHandler := core_http_response.NewHTTPResponseHandler(log, rw)
 
 	log.Debug("Create user handler")
 
 	var request CreateUserRequest
-	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
-		fmt.Println("Error!")
+	if err := core_http_request.DecodeAndValidateRequest(r, &request); err != nil {
+		responseHandler.ErrorResponse(err, "Failed to decode and validate HTTP request")
 	}
 
 	rw.WriteHeader(http.StatusOK)
